a1dd8c505379c70796cc9b45725d3582ae4ccbab
Hussain Shareef <hussain.shareef@gmail.com>
2024-12-16 20:20:48 +0500

    Automatic deployment from local build with 404.html

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,14 @@ dest_dir="./docs"
 # Copy files recursively
 cp -r "$src_dir"/* "$dest_dir"
 
-# Stage all changes (optional, customize if needed)
+# Copy index.html to 404.html
+cp "$src_dir/index.html" "$dest_dir/404.html"
+
+# Stage all changes
 git add .
 
 # Commit changes with a descriptive message
-git commit -m "Automatic deployment from local build"
+git commit -m "Automatic deployment from local build with 404.html"
 
 # Push changes to the remote repository
 git push -u origin main
